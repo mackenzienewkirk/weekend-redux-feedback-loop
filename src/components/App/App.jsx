dcf72e9a2df16d1525c827ddec83ca4787e56fa0
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-01-21 15:09:21 -0600

    created component folders/files and have begun GET route in App.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {useEffect} from 'react';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log('in useEffect');
+    getFeedback();
+  }, []);
+
+  const getFeedback = () => {
+    axios.get('/api/feedback')
+    .then((response) => {
+      console.log('in getFeedback', response.data);
+      dispatch({
+        type: 'SET_FEEDBACK',
+        payload: response.data
+      })
+    })
+    .catch((error) => {
+      console.log('error in GET feedback', error)
+    })
+  }
 
   return (
     <div className='App'>
